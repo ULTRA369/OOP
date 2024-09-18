12d423e48f0fe8132048b93a74ad6c8963aba506
Svetlana Linberg <ya.vgb@list.ru>
2024-09-18 20:13:57 +0300

    updating files category and product

--- src/category.py
+++ src/category.py
@@ -1,17 +1,28 @@
-#from src.product import Product
+from src.product import Product
 
 
 class Category:
-    name: str  # Имя
-    description: str  # Описание
-    products: list  # Cписок товаров категории
-    product_count = 0  # Подсчет продуктов
-    category_count = 0  # Подсчет категорий
+    name: str
+    description: str
+    products: list
+    product_count: int = 0
+    category_count: int = 0
 
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.__products = products
+        Category.category_count += 1
+        Category.product_count += len(products)
 
-def __init__(self, name, description, products):
-    self.name = name
-    self.description = description
-    self.products = products
-    Category.category_count += 1
-    Category.product_count += len(products)
+    @property
+    def products(self):
+        products = ""
+        for product in self.__products:
+            p = f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+            products += p
+        return products
+
+    def add_product(self, product_to_add: Product):
+        self.__products.append(product_to_add)
+        Category.product_count += 1
--- src/product.py
+++ src/product.py
@@ -1,14 +1,50 @@
 class Product:
-    name: str                    # Имя
-    description: str             # Описание
-    price: float                 # Цена
-    quantity: int                # Rоличество в наличии
+    name: str
+    description: str
+    price: float
+    quantity: int
     products_list: list = list()
 
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+        Product.products_list.append(self)
 
-def __init__(self, name, description, price, quantity):
-    self.name = name
-    self.description = description
-    self.price = price
-    self.quantity = quantity
-    Product.products_list.append(self)
+    def __str__(self):
+        return f"{self.__class__.__name__}: {self.name} - {self.description} - {self.__price} - {self.quantity}"
+
+    @classmethod
+    def new_product(cls, product_param):
+        """
+        Принимает словарь с ключами: "name"(название), "description"(описание),
+        "price"(цена), "quantity"(количество на складе)
+        """
+        name, description, price, quantity = product_param.values()
+        for product in cls.products_list:
+            if product.name.lower() == name.lower():
+                product.quantity += quantity
+                if product.price < price:
+                    product.price = price
+                return product
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price > 0:
+            if new_price < self.__price:
+                agreement = input(
+                    "Старая цена товара выше введенной.\n"
+                    "Вы уверены, что хотите снизить цену на товар?\n"
+                    "y - да / n - нет\n"
+                )
+                if agreement.lower() != "y":
+                    return
+            self.__price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
