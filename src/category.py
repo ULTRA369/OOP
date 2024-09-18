016ddb3786638f393c6f0c44fda8ca0ce7ffecfe
Svetlana Linberg <ya.vgb@list.ru>
2024-09-18 19:43:43 +0300

    updating files category and product

--- src/category.py
+++ src/category.py
@@ -4,9 +4,13 @@ class Category:
     name: str                    # Имя
     description: str             # Описание
     products: list               # Cписок товаров категории
-
+    product_count = 0            # Подсчет продуктов
+    category_count = 0           # Подсчет категорий
 
 def __init__(self, name, description, products):
     self.name = name
     self.description = description
     self.products = products
+    Category.category_count += 1
+    Category.product_count += len(products)
+
--- src/product.py
+++ src/product.py
@@ -3,10 +3,11 @@ class Product:
     description: str             # Описание
     price: float                 # Цена
     quantity: int                # Rоличество в наличии
-
+    products_list: list = list()
 
 def __init__(self, name, description, price, quantity):
     self.name = name
     self.description = description
     self.price = price
     self.quantity = quantity
+    Product.products_list.append(self)
